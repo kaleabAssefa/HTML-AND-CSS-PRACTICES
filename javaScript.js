5fee3a330ae4d09781b42f2900fa76cd178d708f
kaleabAssefa <62577860+kaleabAssefa@users.noreply.github.com>
2026-07-04 16:11:39 -0400

    Implement button click events for announcements
    Added event listeners for buttons to create new elements and update styles.

--- javaScript.js
+++ javaScript.js
@@ -20,3 +20,37 @@ console.log(Object.values(employee));
  console.log(Object.entries(employee));
  
  console.log(employee.hasOwnProperty("dept"));
+
+const box = document.getElementById("announcement-box");
+const box2 = document.getElementById("new-announcement-box")
+const button = document.getElementById("update-btn");
+//const button = document.getElementById("update-btn2");
+
+
+button.addEventListener("click", () => {
+
+
+    const newElement = document.createElement("h1");
+
+   
+    newElement.innerText = "JS Practice";
+
+
+    newElement.style.color = "black";
+    newElement.style.background = "red";
+    newElement.style.fontSize = "100px";
+    newElement.style.textAlign = "center";
+    newElement.style.display = "block";
+
+
+    box.appendChild(newElement);
+});
+
+const newButton = document.getElementById("update-btn2");
+newButton.addEventListener("click", () => {
+    newButton.style.color = "white";
+    newButton.style.backgroundColor = "blue";
+    pushButton.innerHTML = "Greetings"
+
+})
+
